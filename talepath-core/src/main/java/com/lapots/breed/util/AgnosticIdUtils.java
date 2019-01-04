d6e2af987c13c0f3b9ed15030fdf93c26e2509cf
lapots <sebastrident@gmail.com>
2019-01-04 21:51:48 +0300

    Updates for id generation

--- talepath-core/src/main/java/com/lapots/breed/util/AgnosticIdUtils.java
+++ talepath-core/src/main/java/com/lapots/breed/util/AgnosticIdUtils.java
@@ -10,12 +10,31 @@ public class AgnosticIdUtils {
 
     /**
      * Generates id.
+     *
+     * Example:
+     *  #1
+     *      Input
+     *          story = Overlord
+     *          character = Albedo
+     *      Output
+     *          id = story_overlord_character_albedo
+     *   #2
+     *      Input
+     *          story = Overlord
+     *          character = Ainz Ooal Gown
+     *      Output
+     *          id = story_overlord_character_ainz_ooal_gown
+     *
+     *  TODO: investigate the way to update names as for id it uses it as a parameter
+     *
      * @param story story name or schema name
      * @param name entity name for which id will be generated
      * @return generated id
      */
     public String generateId(String story, String name) {
-        return String.format("story_%s_character_%s", story, name);
+        return String.format("story_%s_character_%s", story, name)
+            .replaceAll(" ", "_")
+            .toLowerCase();
     }
 
     /**
