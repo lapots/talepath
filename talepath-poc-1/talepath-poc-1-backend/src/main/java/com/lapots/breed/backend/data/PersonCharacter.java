35ca9ff9a8811ed990b3a7cf62e99714bbd2ed41
lapots <sebastrident@gmail.com>
2019-04-06 17:17:21 +0300

    Vert.x REST integration test

--- talepath-poc-1/talepath-poc-1-backend/src/main/java/com/lapots/breed/backend/data/PersonCharacter.java
+++ talepath-poc-1/talepath-poc-1-backend/src/main/java/com/lapots/breed/backend/data/PersonCharacter.java
@@ -2,6 +2,8 @@ package com.lapots.breed.backend.data;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import org.javers.core.metamodel.annotation.Id;
 import org.javers.core.metamodel.annotation.TypeName;
 
@@ -11,6 +13,7 @@ import org.javers.core.metamodel.annotation.TypeName;
 @TypeName("ConfigurablePersonCharacter")
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class PersonCharacter {
     @Id
     private String id;
--- talepath-poc-1/talepath-poc-1-ui/src/testInteg/java/com/lapots/breed/ui/SampleRestVerticleTest.java
+++ talepath-poc-1/talepath-poc-1-ui/src/testInteg/java/com/lapots/breed/ui/SampleRestVerticleTest.java
@@ -1,40 +1,52 @@
 package com.lapots.breed.ui;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.assertj.core.api.Assertions.assertThat;
 
-import org.junit.jupiter.api.BeforeEach;
+import static io.restassured.RestAssured.get;
+
+import com.lapots.breed.backend.data.PersonCharacter;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 
+import io.restassured.RestAssured;
 import io.vertx.core.Vertx;
-import io.vertx.ext.web.client.WebClient;
-import io.vertx.ext.web.codec.BodyCodec;
 import io.vertx.junit5.VertxExtension;
 import io.vertx.junit5.VertxTestContext;
 
 /**
- * Simple integration test.
+ * Sample test for REST.
  */
 @ExtendWith(VertxExtension.class)
 public class SampleRestVerticleTest {
+    private static final String ID = "p_id";
+    private static final String NAME = "Jack";
+    private static final int AGE = 20;
 
-    @BeforeEach
-    void prepare(Vertx vertx, VertxTestContext testContext) {
+    @BeforeAll
+    static void configureRestAssured(Vertx vertx, VertxTestContext testContext) {
+        RestAssured.baseURI = "http://localhost";
+        RestAssured.port = Integer.getInteger("http.port", 8080);
+
         vertx.deployVerticle(SampleRestVerticle.class.getCanonicalName(), testContext.completing());
     }
 
     @Test
-    @DisplayName("Test that server started")
-    void checkServerHasStarted(Vertx vertx, VertxTestContext testContext) {
-        WebClient webClient = WebClient.create(vertx);
-        webClient.get(8080, "localhost", "/")
-            .as(BodyCodec.string())
-            .send(testContext.succeeding(response -> testContext.verify(() -> {
-                assertEquals(200, response.statusCode());
-                assertTrue(response.body().length() > 0);
-                assertTrue(response.body().contains("Sample Vert.x 3 application"));
-                testContext.completeNow();})));
+    @DisplayName("Test GET response")
+    void checkGetCharacter() {
+        PersonCharacter result = get("/api/character")
+            .thenReturn()
+            .as(PersonCharacter.class);
+        assertThat(result.getId()).isEqualTo(ID);
+        assertThat(result.getName()).isEqualTo(NAME);
+        assertThat(result.getAge()).isEqualTo(AGE);
+    }
+
+    @AfterAll
+    static void unconfigureRestAssured() {
+        RestAssured.reset();
     }
 }
