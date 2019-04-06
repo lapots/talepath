5a980f93d656e6fd10930a89db97bfc1f8071c71
lapots <sebastrident@gmail.com>
2019-04-06 14:19:03 +0300

    Removed unused code, added stub unit test

---
+++ talepath-poc-1/talepath-poc-1-backend/src/test/groovy/com/lapots/breed/backend/StubTestSpecification.groovy
@@ -0,0 +1,15 @@
+package com.lapots.breed.backend
+
+import spock.lang.Specification
+
+/**
+ * Stub test.
+ */
+class StubTestSpecification extends Specification {
+
+    def "should always be true"() {
+        expect:
+            assert 1 == 1
+    }
+
+}
