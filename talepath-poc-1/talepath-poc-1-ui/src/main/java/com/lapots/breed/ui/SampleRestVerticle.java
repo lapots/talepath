0d30432bdad962e53a2399bb6cc002fe1dd4c36a
lapots <sebastrident@gmail.com>
2019-04-06 15:20:27 +0300

    Simple vert.x REST

--- talepath-poc-1/talepath-poc-1-ui/src/main/java/com/lapots/breed/ui/SampleRestVerticle.java
+++ talepath-poc-1/talepath-poc-1-ui/src/main/java/com/lapots/breed/ui/SampleRestVerticle.java
@@ -1,8 +1,11 @@
 package com.lapots.breed.ui;
 
+import com.lapots.breed.backend.data.PersonCharacter;
+
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.Future;
 import io.vertx.core.http.HttpServerResponse;
+import io.vertx.core.json.Json;
 import io.vertx.ext.web.Router;
 
 /**
@@ -11,13 +14,19 @@ import io.vertx.ext.web.Router;
 public class SampleRestVerticle extends AbstractVerticle {
 
     @Override
-    public void start(Future<Void> startFuture) throws Exception {
+    public void start(Future<Void> startFuture) {
         Router router = Router.router(vertx);
         router.route("/").handler(routingContext -> {
             HttpServerResponse response = routingContext.response();
             response
                 .putHeader("content-type", "text/html")
                 .end("<h1>Sample Vert.x 3 application</h1>");
+        });
+        router.route("/api/character").handler(routingContext -> {
+            HttpServerResponse response = routingContext.response();
+            response
+                .putHeader("content-type", "application/json; charset=utf-8")
+                .end(Json.encodePrettily(new PersonCharacter("p_id", "Jack", 20)));
         });
         vertx
             .createHttpServer()
